17caa0ab07deb024aaa7948d417fd30f402222b0
dokor <antoine@lelouet.fr>
2024-08-12 19:55:59 +0200

    add dead cards on toString

--- src/main/java/com/lelouet/services/game/beans/Card.java
+++ src/main/java/com/lelouet/services/game/beans/Card.java
@@ -17,7 +17,10 @@ public class Card {
 
     @Override
     public String toString() {
-        return rank + " of " + suit;
+        if(statut.isCanBeUsedForSuite()){
+            return rank + " of " + suit;
+        }
+        return "#" + rank + " of " + suit; // todo : temporaire, a supprimer une fois le front dispo
     }
 
 }
